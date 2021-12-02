ed95d3da8be40d4b7c713acf623c21c3606093c4
Reid Wilbur <reid.wilbur@gmail.com>
2021-12-02 09:44:11 -0600

    Day2 slns

--- src/main/java/com/wilb0t/aoc/Day1.java
+++ src/main/java/com/wilb0t/aoc/Day1.java
@@ -18,8 +18,8 @@ class Day1 {
     var count = 0;
     var window = depths[0] + depths[1] + depths[2];
     for (var i = 1; i < depths.length - 2; i++) {
-      int last = window;
-      window = window - depths[i - 1] + depths[i + 2];
+      var last = window;
+      window += depths[i + 2] - depths[i - 1];
       count += (window > last) ? 1 : 0;
     }
     return count;
---
+++ src/main/java/com/wilb0t/aoc/Day2.java
@@ -0,0 +1,49 @@
+package com.wilb0t.aoc;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class Day2 {
+  public static record Command(String dir, int dist) { }
+ 
+  public static final Function<String, Command> INPUT_MAPPER = (line) ->  {
+    var parts = line.split(" ");
+    var dir = parts[0];
+    var dist = Integer.parseInt(parts[1]);
+    return new Command(dir, dist);
+  };
+  
+  public static int getDistMult(List<Command> commands) {
+    var hz = 0;
+    var ht = 0;
+
+    for (var cmd : commands) {
+      switch (cmd.dir()) {
+        case "forward" -> hz += cmd.dist();
+        case "up" -> ht -= cmd.dist();
+        case "down" -> ht += cmd.dist();
+      }
+    }
+    
+    return hz*ht;
+  }
+  
+  public static int getAimDistMult(List<Command> commands) {
+    var hz = 0;
+    var ht = 0;
+    var aim = 0;
+    
+    for (var cmd : commands) {
+      switch (cmd.dir()) {
+        case "forward" -> {
+          hz += cmd.dist();
+          ht += aim * cmd.dist();
+        }
+        case "up" -> aim -= cmd.dist();
+        case "down" -> aim += cmd.dist();
+      }
+    }
+    
+    return hz*ht;
+  }
+}
